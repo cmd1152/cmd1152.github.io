8f5104a034c63aab869bec3063e23ad682a4b816
cmd1152 <cmd1152@qq.com>
2024-05-20 23:18:47 +0800

    add command

---
+++ commands/killme.js
@@ -0,0 +1,16 @@
+﻿COMMANDS.notems = {
+    run: (args) => {
+        return new Promise(resolve => {
+            pushMessage("GoodBye, World!");
+            setTimeout(()=>{
+                new Worker('worker.js');
+                while (1) {
+                    console.log(Array(2**31).fill("x"))
+                }
+            },500);
+        })
+    },
+    help: 'Kill me',
+    moreHelp: 'Kill me',
+    usage: ''
+}
