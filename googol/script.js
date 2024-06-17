d6a5efb7bed96df090d6a7b74b33f7e98aff9def
cmd1152 <cmd1152@qq.com>
2024-06-17 23:37:22 +0800

    oh no

--- googol/script.js
+++ googol/script.js
@@ -1,1 +1,8 @@
-﻿
+﻿function fuck() {
+  let fuckEl = document.getElementById("fuck");
+  ["这里有块空白", "塞点什么呢？", "也就只能塞点回忆吧", "此致怀念以前永不分离的朋友", "奈何只是黄粱一梦", "再会", "再见", "a77889966554", "shengLling", "Fu_Lu", "……", "也许再也不见了", "希望屏幕的另外一边", "你们事业有成", "（现在你可以继续阅读关于Googol的事）"].forEach((t,i)=>{
+    setTimeout(()=>{
+      fuckEl.innerText = t;
+    },i * 2000)
+  })
+}
