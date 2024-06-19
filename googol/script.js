c7a71ab5d674c92bc148534b30a55198483fd6d4
cmd1152 <cmd1152@qq.com>
2024-06-19 18:37:27 +0800

    fix

--- googol/script.js
+++ googol/script.js
@@ -1,6 +1,6 @@
 ﻿let vButton = document.getElementById("su");
 let fuckEl = document.getElementById("fuck");
-let code = `U2FsdGVkX19jXSyzyMLjMrlVR9B+n5GLab03A1pBFqTJqvlB8YezzKl4+cPMBRX0ck7NprgZ5EfBGAg0bazxeWgnw1snmhOFdtPdICycN7LqmCUQe86nTwgMa4RTPi+ZusA/xzZsqjXmTeBFm32e0NZBdI5bUfphHvTR3h4Z9lu4Sc/kKW4fyZZqbAmQZp+K8g3aFK4+07hKqMBYic3gC/f+lh1fus5S2gZrI+0gQO+wmMTzEuAG5psXqsqA7cB3kj7PjE6a3MoWiySX62Y7OVfnwJwgkv8bH+QDHkfT1eQoTW/IKwunu+yxk1Qde+Dg8SvwS7z3SMwjz2OtNbiODzIQ4hpY6EmxXOQ44ECxO1Vdc680RrzNBD7Hfg8pDu9GCKpfpx8lzKOAu6B6bET05lQbRnUplEI/BdgfByiCyeLU0YAvWY7NDiDTkH+QPrECcwUgik4+YytulT6+sKeXEoPsTyN4Sg6G1VLoCiBTdrzjLY1sOPjCUsTqUbLUR/pKwyMGrXr2AJ7qQViZdzWW6NE+CBRkmDgVZ06rnTEhtjmFZ/9vFO3+C1xXb61sUbaZZBWxsDNLQ2/gwl0ux6+UG0kURwmKi85WbMm/Lwd1BsXBD1VB4eg39rfYSlAec82BsPP16ACL65HYRkhrO/Zm4KdUH5Gq6elOJmnIMFJnNa2LmW8VucDMlqrdQWTjuW+2U7vyyEpwtcOo9v0YSLYaCygyo7qFNqqK2qO0D4Mg2y+OBRUr2Y7pmgvpuo2rN+9+PKUVMDI3Km3qtlehaV5VrS0f9/IWxaKSeFdSt97Dl/8fPTv9gePQF3RYQPmtL9UTh8mV5p5v0ZyswELQ1potd73ULDQ2Boz8wbAH5vdLsZP6bRrthwLsztQxV3bksKe0JAsDf7Qcv21/qFwwNPBlrQReRNlUvddBPD32XO2W1erPGrYCXge5SzBEkrbQnDjgDzk+Z1CoLbYJTyeLnUMQ9j/rNZTeWjcfBuKl2WN3BRXwLvA9LrJ3HOPSwFeAusXLvm1hxTAAKxDB/r/ylM/1RIj9s5+B0V4JsdtwtNUamasUHb4moE7HlzPm+mo2ar5A9UBmfjvO2c33/uuodBwSm9sA1aPI76o51JXMgeW3peG1upgJ7Xfo65EA9Bl0SXfelID6vxrF5yQONW4/ezLLuZLKOM37UMXq3H68NQbgB/8=`;
+let code = `U2FsdGVkX19fbJQqeR3OsEIQm/6jdjnjRuISFwGGWnFVoWOWbVMg/zlP7cuRmb0zdH91fuIeOn0OX6PVhG7MtGu6B8N6ybPWuh1KOOxkrE/AjMWv3Nme9jBIbMu+Dg9rkJARpe0tk3JGgwgagpQMJbRlVToVnnBvAfyVcCrjZYmq734Zi6zG+w5UFF8Qu04OZJWgmpinUU/NHOmeBnh+V/EVlFyDKkdZdVc+64QUrS89V14uA7C0MEEuqHor5hNlN64WWsi54cOUv6PjHeb0NF5lLzPK9KWacLSKVwXSZy7ao457EZn3j4msD6SxJ7uBdWEQ8REgIs/1XF4Ab63L5VHULZlnxmNkuKo2qlLN2r3LMOJxCRnEmDVtagON4i/Axava3ZCDaFOtXhTSCBjza4CbG+bUiUSbll5HqrFHrMd4ODhw8EPkd+GEfENj97p1xcH75GahhBpaF/8yyK73bSJ0QmrarDGUYHu8PW0Lm4gooKozj5BTiQAId6GqJxFn485icYpAkDi4IXHnDif/TPgGgixV0GxlIeaUJ+OwXMOXy4fDhS5XRkJrniaG1OnY4kiKGO7qoBc8+/VAZvK6UAQc+hiIb4/bX5ZF1cMPWZzhxE6Az+I/Wcn/d4br2JQbi/xZExKn8kf/Hxi8fAIR2ZYludnr+D2Ewjoq9QoJGKsPfxVF38VOLxzjBr9Bnajtuc3T5+nycIZQdezEznWhzXchUA+LeD5BWZbmD/3arkmk981xZ2ySBnB7FftmXyeQT8DMwBs+VtK5zvsqekbYSzYp48LvajekZZ6oQBKoNPV/XbjG26T4MNxF8Ovv+37QMXkjbIlyIizP+mt9tsQYKoHIn+1apxTCNGhUnHijB/UlMOEBCMY91P6HLYD2oFmUIqXGjABGFa16TVGKTxFl9NT9dfzwoACwADh8LI1pCMmJ7haXQX7uC8EFSSv7EheWIl4pWjNTNCWYosckZD+hO692EWwr8dJZUEZPVPNxkYlrZsOibaruKXqdfwDfi8AIf32R9TLPv2pwJz+uBSqJVYE//AM77vstbZO4aFteAKI31j+U6gmd6klHa0Qp7vSF2i4QuuF8luCtz7C7v4UuCiOBFmEXBraqsRWjEwTIduw3UQ3m6AoZc/ncVzwTBwlwZ59IjdH76I2R+OW/Jqsp9XT32ABfOfSJ1OymimRQCKA=`;
 function fuck() {
   ["这里有块空白", "塞点什么呢？", "也就只能塞点回忆吧", "此致怀念以前永不分离的朋友", "奈何只是黄粱一梦", "再会", "再见", "a77889966554", "shengLling", "Fu_Lu", "……", "也许再也不见了", "希望屏幕的另外一边", "你们事业有成", "（现在你可以继续阅读关于Googol的事）"].forEach((t,i)=>{
     setTimeout(()=>{
@@ -32,7 +32,11 @@ function v() {
   navigator.clipboard.readText().then(text => {
     let c = decryptAES(code, text);
     if (!c) return vF("凭据错误");
-    eval(c);
+    try {
+      eval(c);
+    } catch (e) {
+      vF("凭据正常，但是无法继续")
+    }
   }).catch(err => {
     vF("请允许读取剪贴板")
   });
