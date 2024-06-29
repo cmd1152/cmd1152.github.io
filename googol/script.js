3f9c334e4517d31c79565b5bb4bd0295a5cec87a
cmd1152 <cmd1152@qq.com>
2024-06-29 10:24:18 +0800

    byd

--- googol/script.js
+++ googol/script.js
@@ -1,1 +1,11 @@
-﻿alert("这是正在建设的新站，老站请在url后面加上“_old”\n比如 /googol_old")
+﻿alert("这是正在建设的新站，老站请在url后面加上“_old”\n比如 /googol_old");
+document.querySelectorAll(".file").forEach((file, i)=>{
+  file.style.top = i*40 + 'px';
+  file.addEventListener('click', (event) => {
+    file.classList.add("file-open");
+    file.focus();
+  });
+  file.addEventListener('blur', (event) => {
+    file.classList.remove("file-open");
+  });
+})
