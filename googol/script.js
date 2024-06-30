8454352d4106d4a7b86c91385a9fa1fa7b2c79d3
cmd1152 <cmd1152@qq.com>
2024-06-30 13:52:20 +0800

    awa

--- googol/script.js
+++ googol/script.js
@@ -1,6 +1,6 @@
 ﻿alert("这是正在建设的新站，老站请在url后面加上“_old”\n比如 /googol_old");
 document.querySelectorAll(".file").forEach((file, i)=>{
-  file.style.top = i*40 + 'px';
+  file.style.top = 50+i*40 + 'px';
   file.addEventListener('click', (event) => {
     file.classList.add("file-open");
   });
